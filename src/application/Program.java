fca496bbc3fbe00bf90f9a748fd50acb4cf38b3a
LeoMart7 <lbartiliano@hotmail.com>
2024-08-01 20:30:45 -0300

    Manuseio de peças capturadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 import chess.ChessPiece;
 import chess.ChessException;
@@ -14,11 +16,12 @@ public class Program {
 		Scanner input = new Scanner(System.in);
 		
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(input);
@@ -31,6 +34,9 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(input);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				if(capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 				
 			} catch (ChessException e) {
 				System.out.print(e.getMessage());
